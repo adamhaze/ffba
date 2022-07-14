ff709588e8269a04d906ad1a756ae499db96591f
adamhaze <adha9240@colorado.edu>
2022-07-14 11:21:04 -0600

    starting routes

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+
+
+// separate folder within 'routes' called api
+//      this folder will define routes to save players and/or delete players from Mongodb
+//      define router.post() and router.get()
+//      index.js will send the routes to the frontend build/html file
+
+// eventually have a users.js in api folder to save indiv users to the database
