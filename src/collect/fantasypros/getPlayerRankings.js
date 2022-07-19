f57ee4777c411a81de4c6e1b17f1d0995da0ab9e
adamhaze <adha9240@colorado.edu>
2022-07-18 20:49:32 -0600

    integrating db

--- src/collect/fantasypros/getPlayerRankings.js
+++ src/collect/fantasypros/getPlayerRankings.js
@@ -1,5 +1,7 @@
 
 const puppeteer = require('puppeteer');
+const db = require('../db/index.js');
+const Player = require('../db/models/player.js');
 
 async function scrape(){
     const browser = await puppeteer.launch({});
@@ -7,10 +9,31 @@ async function scrape(){
 
     // successfully gets first ranking for Jonathan Taylor... yay
     await page.goto('https://www.fantasypros.com/nfl/fantasy-football-rankings.php');
-    var element = await page.waitForSelector('#ranking-table > tbody > tr:nth-child(1) > td:nth-child(3)');
+    let selector = '#ranking-table > tbody > tr:nth-child(1) > td:nth-child(2)';
+    var element = await page.waitForSelector(selector);
+
+    for(var i=1; i < 10; i++){
+        let nameQuery = await page.waitForSelector(`#ranking-table > tbody > tr:nth-child(${i}) > td.player__cell.sticky-cell.sticky-cell-two > div > div.player__name > span.everything-but-mobile.js-sort-field`);
+        var name = await page.evaluate(nameQuery => nameQuery.textContent, nameQuery);
+        Player.find({name: name}).exec(async function(err, response){
+            if (err) throw err;
+            let temp = 0;
+            await Player.findOneAndUpdate({name: name, date: temp}, )
+
+            // TODO:
+            // for each name, use 'selector' query (iterate over nth-chile($i)) to get each ranking
+            // map column number to expert
+            // add expert rank stats to Player model
+
+        })
+
+        console.log(name);
+    }
+    
+    
+
     // td:nth-child(2) would be player info(name)
-    var text = await page.evaluate(element => element.textContent, element);
-    console.log(text);
+    
     process.exit();
 }
 
