ffa530feabdd3049a8ed67724c1a77388b7a7cfd
Adam Hayes <adha9240@colorado.edu>
2022-07-20 11:43:37 -0600

    updating change collection

--- src/analyze/analyzePlayerData.js
+++ src/analyze/analyzePlayerData.js
@@ -7,45 +7,51 @@ const Player = require('../db/models/player.js');
 const Change = require('../db/models/change.js');
 const fs = require('fs');
 
+// Player.find({}).then(async function(response){
+//     console.log(response.length);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+// TODO: make this whole thing an async function
+// TODO: figure out how to not just save new players each time, but update 
+// (maybe delete all and start over every time?)
+
 let espnPlayers = '/Users/adamhayes/ws_home/projects/fantasyfb-analysis/src/collect/espn/espn_player_ids.txt'
 const playerMap = fs.readFile(espnPlayers, 'utf8', async function (err, data) {
+    await Change.deleteMany({});
     if (err) throw err;
     let playerListUnq = data.split('\n'); // list of each indiv player in ESPN database ['name, playerId']
     for(var i=0; i < playerListUnq.length; i++){
         let player = playerListUnq[i].split(',');
         let query = {'name': player[0], 'playerId': parseInt(player[1].trim())};
-        Player.find(query).exec(async function(err, response){
-            if (err) throw err;
-            
+        await Player.find(query).then(async function(response){
             // current day would be player at response[response.length-1]
             let n = response.length-1;
-            let query = {'name': response[n].name, 'date': response[n].date};
-            console.log(query);
+            if (n >= 0){
+                // initialize an update object with only the 'new' stats
+                let playerUpdateTemp = {
+                    name: response[n].name,
+                    date: response[n].date,
+                    ADPOvr: (response[0].ADP - response[n].ADP),
+                    ADP1day: n >= 1 ? (response[n-1].ADP - response[n].ADP) : 0,
+                    ADP7day: n >= 6 ? (response[n-6].ADP - response[n].ADP) : 0,
+                    ownOvr: (response[0].owned - response[n].owned),
+                    projRushYds: (response[0].projRushYds - response[n].projRushYds),
+                    projRushTD: (response[0].projRushTD - response[n].projRushTD),
+                    projRecYds: (response[0].projRecYds - response[n].projRecYds),
+                    projRecTD: (response[0].projRecTD - response[n].projRecTD),
+                    projReceptions: (response[0].projReceptions - response[n].projReceptions),
+                    projRushYds7day: n >= 6 ? (response[n-6].projRushYds - response[n].projRushYds) : 0,
+                    projRushTD7day: n >= 6 ? (response[n-6].projRushTD - response[n].projRushTD) : 0,
+                    projRecYds7day: n >= 6 ? (response[n-6].projRecYds - response[n].projRecYds) : 0,
+                    projRecTD7day: n >= 6 ? (response[n-6].projRecTD - response[n].projRecTD) : 0,
+                    projReceptions7day: n >= 6 ? (response[n-6].projReceptions - response[n].projReceptions) : 0
+                };
 
-            // TODO: test analyzePlayerData w/ new format
-
-            // initialize an update object with only the 'new' stats
-            let playerUpdateTemp = {
-                name: response[n].name,
-                date: response[n].date,
-                ADPOvr: (response[0].ADP - response[n].ADP),
-                ADP1day: n >= 1 ? (response[n-1].ADP - response[n].ADP) : 0,
-                ADP7day: n >= 6 ? (response[n-6].ADP - response[n].ADP) : 0,
-                ownOvr: (response[0].owned - response[n].owned),
-                projRushYds: (response[0].projRushYds - response[n].projRushYds),
-                projRushTD: (response[0].projRushTD - response[n].projRushTD),
-                projRecYds: (response[0].projRecYds - response[n].projRecYds),
-                projRecTD: (response[0].projRecTD - response[n].projRecTD),
-                projReceptions: (response[0].projReceptions - response[n].projReceptions),
-                projRushYds7day: n >= 6 ? (response[n-6].projRushYds - response[n].projRushYds) : 0,
-                projRushTD7day: n >= 6 ? (response[n-6].projRushTD - response[n].projRushTD) : 0,
-                projRecYds7day: n >= 6 ? (response[n-6].projRecYds - response[n].projRecYds) : 0,
-                projRecTD7day: n >= 6 ? (response[n-6].projRecTD - response[n].projRecTD) : 0,
-                projReceptions7day: n >= 6 ? (response[n-6].projReceptions - response[n].projReceptions) : 0
+                let playerUpdate = new Change(playerUpdateTemp);
+                await playerUpdate.save();
             };
-
-            let playerUpdate = new Change(playerUpdateTemp);
-            await playerUpdate.save();
 
             // compute overall stat changes
             // playerUpdate.cADPOvr = (response[0].ADP - response[n].ADP); // -adpChange = falling, +adpChange = rising
@@ -72,9 +78,12 @@ const playerMap = fs.readFile(espnPlayers, 'utf8', async function (err, data) {
 
             // TODO: determine to use findOneAndUpdate or just save()
             // await Change.findOneAndUpdate(query, playerUpdate, {upsert: true});
-            process.exit();
+            // process.exit();
+        }).catch(error => {
+            console.log(error);
         });
     }
+    process.exit();
 
 });
 
