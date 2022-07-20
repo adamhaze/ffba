859e93abcda9cf62ebfb2e616bfa9fe1d43e87a4
Adam Hayes <adha9240@colorado.edu>
2022-07-19 19:50:39 -0600

    integrating new model

--- src/analyze/analyzePlayerData.js
+++ src/analyze/analyzePlayerData.js
@@ -4,6 +4,7 @@
 
 var db = require('../db/index.js');
 const Player = require('../db/models/player.js');
+const Change = require('../db/models/change.js');
 const fs = require('fs');
 
 let espnPlayers = '/Users/adamhayes/ws_home/projects/fantasyfb-analysis/src/espn/espn_player_ids.txt'
@@ -20,26 +21,24 @@ const playerMap = fs.readFile(espnPlayers, 'utf8', (err, data) => {
             let n = response.length-1;
             let query = {'name': response[n].name, 'date': response[n].date};
 
-            // TODO: make separate mongoose model schema for PlayerChange
-            // store all change in stats/rankings for each player with their name and date
+            // TODO: test analyzePlayerData w/ new format
 
             // initialize an update object with only the 'new' stats
             let playerUpdate = {
-                cADPOvr: (response[0].ADP - response[n].ADP),
-                cADP1day: n >= 1 ? (response[n-1].ADP - response[n].ADP) : 0,
-                cADP7day: n >= 6 ? (response[n-6].ADP - response[n].ADP) : 0,
-                cOwnOvr: (response[0].owned - response[n].owned),
-                cProjRushYds: (response[0].projRushYds - response[n].projRushYds),
-                cProjRushTD: (response[0].projRushTD - response[n].projRushTD),
-                cProjRecYds: (response[0].projRecYds - response[n].projRecYds),
-                cProjRecTD: (response[0].projRecTD - response[n].projRecTD),
-                cProjReceptions: (response[0].projReceptions - response[n].projReceptions),
-                cProjRushYds7day: n >= 6 ? (response[n-6].projRushYds - response[n].projRushYds) : 0,
-                cProjRushTD7day: n >= 6 ? (response[n-6].projRushTD - response[n].projRushTD) : 0,
-                cProjRecYds7day: n >= 6 ? (response[n-6].projRecYds - response[n].projRecYds) : 0,
-                cProjRecTD7day: n >= 6 ? (response[n-6].projRecTD - response[n].projRecTD) : 0,
-                cProjReceptions7day: n >= 6 ? (response[n-6].projReceptions - response[n].projReceptions) : 0
-
+                ADPOvr: (response[0].ADP - response[n].ADP),
+                ADP1day: n >= 1 ? (response[n-1].ADP - response[n].ADP) : 0,
+                ADP7day: n >= 6 ? (response[n-6].ADP - response[n].ADP) : 0,
+                ownOvr: (response[0].owned - response[n].owned),
+                projRushYds: (response[0].projRushYds - response[n].projRushYds),
+                projRushTD: (response[0].projRushTD - response[n].projRushTD),
+                projRecYds: (response[0].projRecYds - response[n].projRecYds),
+                projRecTD: (response[0].projRecTD - response[n].projRecTD),
+                projReceptions: (response[0].projReceptions - response[n].projReceptions),
+                projRushYds7day: n >= 6 ? (response[n-6].projRushYds - response[n].projRushYds) : 0,
+                projRushTD7day: n >= 6 ? (response[n-6].projRushTD - response[n].projRushTD) : 0,
+                projRecYds7day: n >= 6 ? (response[n-6].projRecYds - response[n].projRecYds) : 0,
+                projRecTD7day: n >= 6 ? (response[n-6].projRecTD - response[n].projRecTD) : 0,
+                projReceptions7day: n >= 6 ? (response[n-6].projReceptions - response[n].projReceptions) : 0
             };
 
             // compute overall stat changes
@@ -65,7 +64,8 @@ const playerMap = fs.readFile(espnPlayers, 'utf8', (err, data) => {
             //     let cProjReceptions7day = (response[n-7].projReceptions - response[n].projReceptions);
             // }
 
-            await Player.findOneAndUpdate(query, playerUpdate, {upsert: true});
+            // TODO: determine to use findOneAndUpdate or just save()
+            await Change.findOneAndUpdate(query, playerUpdate, {upsert: true});
             process.exit();
         });
     }
