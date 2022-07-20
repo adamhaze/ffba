404ea73ed977c363bcfa8c60dbbc37083dcef3dd
Adam Hayes <adha9240@colorado.edu>
2022-07-19 18:48:23 -0600

    success writing FP rankings to local db

--- src/collect/fantasypros/getPlayerRankings.js
+++ src/collect/fantasypros/getPlayerRankings.js
@@ -11,23 +11,22 @@ async function scrape(){
     await page.goto('https://www.fantasypros.com/nfl/fantasy-football-rankings/ppr-overall.php'); // PPR rankings
     
     // iterate through each player on rankings page
-    for(var i=1; i < 5; i++){
+    let numPlayersFP = 449;
+    for(var i=1; i <= numPlayersFP; i++){
         let nameQuery = await page.waitForSelector(`#ranking-table > tbody > tr:nth-child(${i}) > td.player__cell.sticky-cell.sticky-cell-two > div > div.player__name > span.everything-but-mobile.js-sort-field`);
         var name = await page.evaluate(nameQuery => nameQuery.textContent, nameQuery);
-        console.log(name);
 
-        // iterate through each ranking for each player
+        // iterate through rankings for each player
         let ranks = new Array(5);
         for (var j=3; j<=7; j++){
             let rankQuery = await page.waitForSelector(`#ranking-table > tbody > tr:nth-child(${i}) > td:nth-child(${j})`);
             ranks[j-3] = await page.evaluate(rankQuery => rankQuery.textContent, rankQuery);
-            console.log(ranks);
+            if (ranks[j-3] == '-'){ ranks[j-3] = -1};
         };
         let playerUpdate = {rankFPBrown:ranks[0], rankFPErickson:ranks[1], rankFPFitz:ranks[2], rankFPFreedman:ranks[3], rankFPPisapia:ranks[3]};
         
-        // TODO: query player by name and today's date, update rankings
-        let temp = 0;
-        await Player.findOneAndUpdate({name: name, date: temp}, )
+        // Update Player in database with new rankings
+        await Player.findOneAndUpdate({name: name, date: new Date(Date.now()).toDateString()}, playerUpdate, {upsert: true});
     }
     process.exit();
 }
