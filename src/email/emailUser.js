1d2c7e61dd974c42f3f0e36ea38995348aac482b
Adam Hayes <adha9240@colorado.edu>
2022-07-20 18:05:12 -0600

    integrating email

--- src/email/emailUser.js
+++ src/email/emailUser.js
@@ -1,25 +1,30 @@
 var nodemailer = require('nodemailer');
 const credentials = require('./credentials');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: credentials.email,
-    pass: credentials.pw
-  }
-});
 
-var mailOptions = {
-  from: credentials.email,
-  to: credentials.email,
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+async function email (body){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: credentials.email,
+          pass: credentials.pw
+        }
+      });
+      
+    var mailOptions = {
+        from: credentials.email,
+        to: credentials.email,
+        subject: 'Sending Email using Node.js',
+        text: body
+    };
+      
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+email('wassssupppp');
