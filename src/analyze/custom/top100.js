f5df6e577926bd928173447881a5dcd8e0df10b3
adamhaze <adha9240@colorado.edu>
2022-07-24 13:36:52 -0600

    starting top100

--- src/analyze/custom/top100.js
+++ src/analyze/custom/top100.js
@@ -3,7 +3,7 @@ var Ranks = require('../../db/models/ranks.js');
 
 // players sorted in order of avg ranking and their corresponding
 // difference between ranking and ESPN ADP
-
 async function getTop100() {
-
+    let query = {date: new Date(Date.now()).toDateString()};
+    let response = await Ranks.find(query).sort({avg: -1});
 }
