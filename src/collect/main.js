d47608868ae7a09b8f215fcefd7ebf38d01a3792
Adam Hayes <adha9240@colorado.edu>
2022-07-27 19:05:08 -0600

    adding clear data for current day option

--- src/collect/main.js
+++ src/collect/main.js
@@ -1,10 +1,14 @@
 var db = require('../db/index.js');
 var stats = require('./stats/espn.js');
 var rankings = require('./rankings/driver.js');
+var Player = require('../db/models/player.js');
+var Ranks = require('../db/models/ranks.js');
 
 
 let ESPNleagueId = 1807663261;
 (async () => {
+    await Player.deleteMany({date: new Date(Date.now()).toDateString()});
+    await Ranks.deleteMany({date: new Date(Date.now()).toDateString()});
     await stats(ESPNleagueId);
     await rankings();
 })();
