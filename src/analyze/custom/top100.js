3faa43d26a37b364f928ece3c579cfdd98949fea
Adam Hayes <adha9240@colorado.edu>
2022-07-27 19:16:47 -0600

    progress

--- src/analyze/custom/top100.js
+++ src/analyze/custom/top100.js
@@ -1,9 +1,15 @@
 var db = require('../../db/index.js');
 var Ranks = require('../../db/models/ranks.js');
 
-// players sorted in order of avg ranking and their corresponding
-// difference between ranking and ESPN ADP
+// TODO: return players sorted in order of avg ranking 
+// and sorted by ADP for comparison (bool-triggered)
 async function getTop100() {
     let query = {date: new Date(Date.now()).toDateString()};
-    let response = await Ranks.find(query).sort({avg: -1});
+    let response = await Ranks.find(query).sort({avg: 1}); // response[0] = highest avg rank
+    for (var i=0; i < response.length; i++){
+        console.log(response[i].name, ' ', response[i].avg);
+    }
+    
 }
+
+getTop100();
