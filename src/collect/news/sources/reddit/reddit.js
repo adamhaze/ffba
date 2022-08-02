3977fb450487c27dabf96afaa9024a71ec82cba0
adamhaze <adha9240@colorado.edu>
2022-08-02 15:02:17 -0600

    exporting function

--- src/collect/news/sources/reddit/reddit.js
+++ src/collect/news/sources/reddit/reddit.js
@@ -2,6 +2,7 @@
 const snoowrap = require('snoowrap');
 const credentials = require('./credentials.js');
 
+const subreddits = ['fantasyfootball', 'Fantasy_Football', 'DynastyFF'];
 const r = new snoowrap({
     userAgent: 'something',
     clientId: credentials.clientId,
@@ -9,6 +10,18 @@ const r = new snoowrap({
     username: credentials.username,
     password: credentials.password
 });
-var newPosts = r.getSubreddit('fantasyfootball').getNew({limit: 50}).then(async function (response){
-    console.log(response.length);
-});
+
+async function getNews(){
+
+    var newPosts = await r.getSubreddit(subreddits[0]).getNew({limit: 50});
+    console.log(newPosts);
+    
+    
+    // r.getSubreddit('fantasyfootball').getNew({limit: 50}).then(async function (response){
+    //     console.log(response.length);
+    // });
+
+};
+
+
+module.exports = getNews;
