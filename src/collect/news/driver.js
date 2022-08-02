dd18190236cd74959b6e304e3dd612578a0e9a8d
Adam Hayes <adha9240@colorado.edu>
2022-08-02 10:04:19 -0600

    starting reddit news scraper

---
+++ src/collect/news/driver.js
@@ -0,0 +1,2 @@
+// this script will run the news scrapers from multiple different sources
+reddit = require('./sources/reddit/reddit.js');
---
+++ src/collect/news/sources/reddit/reddit.js
@@ -0,0 +1,11 @@
+// https://not-an-aardvark.github.io/snoowrap/index.html
+const snoowrap = require('snoowrap');
+const credentials = require('./credentials.js');
+
+const r = new snoowrap({
+    userAgent: 'something',
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    username: credentials.username,
+    password: credentials.password
+});
