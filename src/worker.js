7a8c1f74635f9fdcc08f4374b7110638c289b2ad
adamhaze <adha9240@colorado.edu>
2022-08-12 16:25:17 -0600

    todo

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,11 @@
 const collect = require('./collect/main.js');
 const analyze = require('./analyze/main.js');
 
+// TODO:
+// const schedule = require('node-schedule');
+// const job = schedule.scheduleJob('*/1 * * * *', function() {
+//   requestListing();
+// });
 
 (async () => {
     await collect();
