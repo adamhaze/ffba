9d0f2569e0fd83746ca804b90b9902f035260205
Adam Hayes <adha9240@colorado.edu>
2023-03-07 19:21:28 -0700

    init

---
+++ ui/src/auth/SplashPage.js
@@ -0,0 +1,18 @@
+import './SplashPage.css';
+
+function SplashPage() {
+
+    
+	return (
+		<div className="Splash">
+			<header className="splash-header">
+				what up 
+				<div>
+					🍗
+				</div>
+			</header>
+		</div>
+	);
+}
+
+export default SplashPage;
